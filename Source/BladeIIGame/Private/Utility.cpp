0890aa1a18703a4f6b217c5291a041c80d30ba22
James Stanton <jestanton@outlook.com>
2020-02-11 20:34:09 +0000

    Tweaked formatting

--- Source/BladeIIGame/Private/Utility.cpp
+++ Source/BladeIIGame/Private/Utility.cpp
@@ -3,19 +3,19 @@
 const void Utility::LogInfo(const FString& message)
 {
 #if QDEBUG
-	UE_LOG(LogTemp, Warning, TEXT("[INFO   ]%s %s"), *Utility::GetTimestamp(), *message);
-#endif // QDEBUG
+	UE_LOG(LogTemp, Warning, TEXT("[INFO   ][%s] %s"), *Utility::GetTimestamp(), *message);
+#endif
 }
 
 const void Utility::LogWarning(const FString& message)
 {
 #if QDEBUG
-	UE_LOG(LogTemp, Warning, TEXT("[WARNING]%s %s"), *Utility::GetTimestamp(), *message);
-#endif // QDEBUG
+	UE_LOG(LogTemp, Warning, TEXT("[WARNING][%s] %s"), *Utility::GetTimestamp(), *message);
+#endif
 }
 
 const FString Utility::GetTimestamp()
 {
 	auto utcnow = FDateTime::UtcNow();
-	return FString::Printf(TEXT("[%02d:%02d:%02d:%03d]"), utcnow.GetHour(), utcnow.GetMinute(), utcnow.GetSecond(), utcnow.GetMillisecond());
+	return FString::Printf(TEXT("%02d:%02d:%02d:%03d"), utcnow.GetHour(), utcnow.GetMinute(), utcnow.GetSecond(), utcnow.GetMillisecond());
 }
