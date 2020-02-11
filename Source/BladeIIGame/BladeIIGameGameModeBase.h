a509c49bf525eaf961b025eefd2289446887148e
James Stanton <jestanton@outlook.com>
2020-02-11 16:56:04 +0000

    added empty unreal project

---
+++ Source/BladeIIGame/BladeIIGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BladeIIGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLADEIIGAME_API ABladeIIGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
