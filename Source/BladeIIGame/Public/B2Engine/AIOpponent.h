7412c91b83725591e3aaa4ff84b204cce0eee5e3
James Stanton <jestanton@outlook.com>
2020-02-14 23:08:46 +0000

    Added derived classes for opponents

---
+++ Source/BladeIIGame/Public/B2Engine/AIOpponent.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Opponent.h"
+
+class B2AIOpponent : public UB2Opponent
+{
+
+};
---
+++ Source/BladeIIGame/Public/B2Engine/NetOpponent.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Opponent.h"
+
+UCLASS()
+class UB2NetOpponent : public UB2Opponent
+{
+
+};
