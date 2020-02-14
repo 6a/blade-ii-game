14f17c04cf4fafc1a19785ed16f94106ca1566e9
James Stanton <jestanton@outlook.com>
2020-02-14 23:08:51 +0000

    Added derived classes for servers

---
+++ Source/BladeIIGame/Public/B2Engine/AIServer.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Server.h"
+
+class B2AIServer : public B2Server
+{
+
+};
---
+++ Source/BladeIIGame/Public/B2Engine/NetServer.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Server.h"
+
+class B2NetServer : public B2Server
+{
+
+};
