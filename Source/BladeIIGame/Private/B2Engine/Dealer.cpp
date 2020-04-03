a8f00d194000e8d38ee3258b818cccd1d269f748
James Stanton <jestanton@outlook.com>
2020-04-03 03:41:53 +0100

    Working on deal

--- Source/BladeIIGame/Private/B2Engine/Dealer.cpp
+++ Source/BladeIIGame/Private/B2Engine/Dealer.cpp
@@ -7,6 +7,7 @@
 const size_t DECK_CAPACITY = 15;
 const size_t HAND_CAPACITY = 10;
 const size_t FIELD_CAPACITY = 15;
+const float CARD_HEIGHT = 0.03f;
 
 void B2Dealer::Deal()
 {
@@ -18,15 +19,23 @@ void B2Dealer::Deal()
 
 	check(Arena);
 
+	bCardsDealt = true;
+
 	// Const settings
 	float DelayOnStart = 1.5f;
 	float DelayOffsetOnStart = 0.2f;
 	float DelayOOBToDeck = 0.4f;
 	float DelayPreDeal = 1.f;
-	float DelayPostDeal = 16.f;
-	float DelayShowPlayerCards = 0.4f;
+	float DelayPostDeal = 7.5f;
+	float DelayPostReveal = 3.f;
+	float DelayPlayerPerCardShuffle = 0.25f;
+	float DelayOpponentPerCardShuffle = 0.1f;
+	float DelayPreOpponentShuffle = 0.7f;
+	float SubtractDelayShowPlayerCards = 0.4f;
 	float DurationDeckToAir = 0.4f;
 	float DurationAirToHand = 0.3f;
+	float DurationRevealCards = 0.3f;
+	float DurationGroupCards = 0.5f;
 
 	// Player cards - into deck
 	for (size_t i = 0; i < DECK_CAPACITY; i++)
@@ -135,7 +144,7 @@ void B2Dealer::Deal()
 	// Player cards reveal
 	for (size_t i = 0; i < Arena->PlayerHand->Size(); i++)
 	{
-		float Delay = DelayPostDeal - (i * DelayShowPlayerCards);
+		float Delay = DelayPostDeal - (i * SubtractDelayShowPlayerCards);
 
 		// Transition 1
 		FVector StartPosition = Arena->PlayerHandReversed->GetTransformForIndex(i).Position;
@@ -147,9 +156,54 @@ void B2Dealer::Deal()
 		ACard* Card = Arena->PlayerHand->GetCardByIndex(i);
 
 		// Add the transition to the transition queue
-		B2Transition Transition = B2Transition(StartPosition, EndPosition, StartRotation, EndRotation, FVector(4.5f, 2.f, 5.f), EEase::EaseIn, 0.2f, Delay);
+		B2Transition Transition = B2Transition(StartPosition, EndPosition, StartRotation, EndRotation, FVector(4.5f, 2.f, 5.f), EEase::EaseIn, DurationRevealCards, Delay);
 		Card->QueueTransition(Transition);
 	}
 
-	bCardsDealt = true;
+	// Player cards shuffle
+	for (size_t i = 0; i < Arena->PlayerHand->Size(); i++)
+	{
+		float Delay = DelayPostReveal - (i * (DelayPlayerPerCardShuffle));
+		FVector CenterPosition = FMath::Lerp(Arena->PlayerHand->GetTransformForIndex(4).Position, Arena->PlayerHand->GetTransformForIndex(5).Position, 0.5f) + (i * FVector(0, 0, CARD_HEIGHT));
+		FRotator CenterRotation = FMath::Lerp(Arena->PlayerHand->GetTransformForIndex(4).Rotation, Arena->PlayerHand->GetTransformForIndex(5).Rotation, 0.5f);
+
+		// Transition 1
+		FVector StartPosition = Arena->PlayerHand->GetTransformForIndex(i).Position;
+		FRotator StartRotation = Arena->PlayerHand->GetTransformForIndex(i).Rotation;
+		FVector EndPosition = CenterPosition;
+		FRotator EndRotation = CenterRotation;
+
+		// Get a pointer to the target card 
+		ACard* Card = Arena->PlayerHand->GetCardByIndex(i);
+
+		// Add the transition to the transition queue
+		B2Transition Transition = B2Transition(StartPosition, EndPosition, StartRotation, EndRotation, FVector(0, 0, 0), EEase::EaseInOut, DurationGroupCards, Delay);
+		Card->QueueTransition(Transition);
+	}
+
+	// Opponents cards shuffle
+	for (size_t i = 0; i < Arena->PlayerHand->Size(); i++)
+	{
+		float Delay = DelayPostDeal + DelayPreOpponentShuffle - (i * (SubtractDelayShowPlayerCards + DelayOpponentPerCardShuffle));
+		FVector CenterPosition = FMath::Lerp(Arena->OpponentHand->GetTransformForIndex(4).Position, Arena->OpponentHand->GetTransformForIndex(5).Position, 0.5f);
+		CenterPosition += (i * FVector(0, 0, CARD_HEIGHT));
+		FRotator CenterRotation = FMath::Lerp(Arena->OpponentHand->GetTransformForIndex(4).Rotation, Arena->OpponentHand->GetTransformForIndex(5).Rotation, 0.5f);
+
+		// Transition 1
+		FVector StartPosition = Arena->OpponentHand->GetTransformForIndex(i).Position;
+		FRotator StartRotation = Arena->OpponentHand->GetTransformForIndex(i).Rotation;
+		FVector EndPosition = CenterPosition;
+		FRotator EndRotation = CenterRotation;
+
+		// Get a pointer to the target card 
+		ACard* Card = Arena->OpponentHand->GetCardByIndex(i);
+
+		// Add the transition to the transition queue
+		B2Transition Transition = B2Transition(StartPosition, EndPosition, StartRotation, EndRotation, FVector(0, 0, 0), EEase::EaseInOut, DurationGroupCards, Delay);
+		Card->QueueTransition(Transition);
+	}
+
+	// At this point we can assume that all the cards are relatively in sync
+
+
 }
