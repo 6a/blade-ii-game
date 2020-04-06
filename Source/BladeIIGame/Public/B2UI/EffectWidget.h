d1c2bcb533e8b6b5713ebd5156e42b25acbfa516
James Stanton <jestanton@outlook.com>
2020-04-06 15:58:23 +0100

    Added base Effect widget class

---
+++ Source/BladeIIGame/Public/B2UI/EffectWidget.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "EffectWidget.generated.h"
+
+UCLASS()
+class BLADEIIGAME_API UEffectWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
